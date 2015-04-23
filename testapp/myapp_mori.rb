036a30a57992cf8c5f495681fdb58492d92e3737
root <root@debian.localdomain>
2015-04-23 11:59:37 +0900

    別ファイルを参照できるようにした myapp.rb => myapp_mori.rb

--- testapp/myapp_mori.rb
+++ testapp/myapp_mori.rb
@@ -1,52 +1,15 @@
 
 require 'rubygems'
-require 'sinatra/base'
+require 'sinatra'
 require 'sinatra/reloader'
 
 # require 'sinatra/reloader'
 
 require_relative 'users'
 
-class Myapp < Sinatra::Base
-set :bind, '0.0.0.0'
-
-@@err_msg = ""
-
-get '/' do
-  erb :index
-end
-
-post '/list' do
-  erb :list
-end
-
 post '/attend' do
     @id = params[:id].to_i
+ #   @pass = params[:pass].to_s
+    @pass = "maki chan kawaii ka ki ku ke ko"
     erb :attend
-=begin
-  id = params[:id].to_i
-  if Userslist.include?(id)
-    @id = id
-    @name = Userslist.access(id)
-    @exists = true
-  else
-    @exists = false
-  end
-=end
-end
-
-get '/register' do
-  erb :reg
-end
-
-post '/reg_finish' do 
-  @newid = params[:id].to_i
-  @newname = params[:name]
-  if Userslist.add(@newid, @newname)
-    erb :reg_finish
-  else
-    erb :reg_error
-  end
-end
-  run! if app_file == $0
 end
