59d5bbb3e933f8959eec7e595a094d3bebdea515
root <root@debian.localdomain>
2015-04-23 10:04:32 +0900

    update database information

--- testapp/user.rb
+++ testapp/user.rb
@@ -6,9 +6,9 @@ require 'mysql2'
 ActiveRecord::Base.establish_connection(
   adapter: "mysql2",
   host:    "localhost",
-  username: "ko",
-  password: "hemmi",
-  database: "test",
+  username: "root",
+  password: "password",
+  database: "test_db",
 )
 
 # id: integer, name: varchar
