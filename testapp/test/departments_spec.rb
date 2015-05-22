7c3ca4c9eca68bf881b8101388cade6e0be4c440
ko <ko@khenmi.mydomain>
2015-05-22 16:46:34 +0900

    モデルのModuleが変わったのにしたがってtestを修正

--- testapp/test/departments_spec.rb
+++ testapp/test/departments_spec.rb
@@ -3,7 +3,7 @@ require_relative '../model/_entity/database_information'
 require_relative '../model/departments'
 require_relative '../model/users'
 
-describe Departments do
+describe Model::Departments do
   let(:id)       { 9876 }
   let(:name)          { 'anonymous' }
   let(:department_id) { 1 }
@@ -12,51 +12,51 @@ describe Departments do
 
   context do
     after do
-      User.destroy_all
-      Department.destroy_all
+      Model::User.destroy_all
+      Model::Department.destroy_all
     end
 
     context 'when adding a new department' do
       it 'accepts valid inputs' do
-        expect(Departments.add(department)).to be true
+        expect(Model::Departments.add(department)).to be true
       end
 
       it 'rejects a duplicated name' do
-        Departments.add(department)
-        expect(Departments.add(department)).to be false
+        Model::Departments.add(department)
+        expect(Model::Departments.add(department)).to be false
       end
 
       it 'rejects an empty name' do
-        expect(Departments.add('')).to be false
+        expect(Model::Departments.add('')).to be false
       end
     end
 
     context 'when there exists a department' do
       before do
-        Department.create(id: department_id, name: department)
+        Model::Department.create(id: department_id, name: department)
       end
 
       context 'when deleting a department' do
         it 'succeeds with a valid department' do
-          expect(Departments.remove(department_id)).to be true
+          expect(Model::Departments.remove(department_id)).to be true
         end
 
         it 'fails if the department does not exist' do
-          expect(Departments.remove(department_id + 1)).to be false
+          expect(Model::Departments.remove(department_id + 1)).to be false
         end
 
         after do
-          Department.destroy_all
+          Model::Department.destroy_all
         end
       end
 
       context 'when there exists a user' do
         before do
-          Users.add(id, name, department_id, password)
+          Model::Users.add(id, name, department, password)
         end
 
         it 'returns users belongs to it' do
-          expect(Departments.users(department_id)).not_to be nil
+          expect(Model::Departments.users(department_id)).not_to be nil
         end
       end
     end
--- testapp/test/users_spec.rb
+++ testapp/test/users_spec.rb
@@ -1,8 +1,8 @@
 
-require_relative '../model/_entity/database_information'
-require_relative '../model/users'
+require './model/_entity/database_information'
+require './model/users'
 
-describe Users do
+describe Model::Users do
   let(:id)       { 9876 }
   let(:name)          { 'anonymous' }
   let(:department_id) { 1 }
@@ -11,80 +11,80 @@ describe Users do
 
   context do
     before do
-      Department.create(id: department_id, name: department)
+      Model::Department.create(id: department_id, name: department)
     end
 
     after do
-      Timecard.destroy_all
-      User.destroy_all
-      Department.destroy_all
+      Model::Timecard.destroy_all
+      Model::User.destroy_all
+      Model::Department.destroy_all
     end
 
     context 'when adding a new user' do
       it 'accepts valid inputs' do
-        expect(Users.add(id, name, department_id, password)).to be true
+        expect(Model::Users.add(id, name, department, password)).to be true
       end
 
       it 'soundly accepts the user' do
-        if Users.add(id, name, department_id, password)
-          expect(User.exists?(id)).to be true
+        if Model::Users.add(id, name, department, password)
+          expect(Model::User.exists?(id)).to be true
         end
       end
 
       it 'rejects a duplicated id' do
-        Users.add(id, name, department_id, password)
-        expect(Users.add(id, name, department_id, password)).to be false
+        Model::Users.add(id, name, department, password)
+        expect(Model::Users.add(id, name, department, password)).to be false
       end
 
       it 'accepts a duplicated name' do
-        Users.add(id, name, department_id, password)
+        Model::Users.add(id, name, department, password)
         another_id = id + 1
-        expect(Users.add(another_id, name, department_id, password)).to be true
+        expect(Model::Users.add(another_id, name, department, password)).to be true
       end
 
       it 'rejects an empty name' do
-        expect(Users.add(id, '', department_id, password)).to be false
+        expect(Model::Users.add(id, '', department, password)).to be false
       end
 
       it 'rejects a long name' do
         long_name = 'a' * 51
-        expect(Users.add(id, long_name, department_id, password)).to be false
+        expect(Model::Users.add(id, long_name, department, password)).to be false
       end
 
       it 'rejects if the department does not exist' do
-        expect(Users.add(id, name, department_id + 1, password)).to be false
+        expect(Model::Users.add(id, name, department + '1', password)).to be false
       end
     end
 
     context 'if there exists a user and' do
       before do
-        Users.add(id, name, department_id, password)
+        Model::Users.add(id, name, department, password)
       end
 
       context 'when deleting' do
         it 'succeeds with a valid user' do
-          expect(Users.remove(id)).to be true
+          expect(Model::Users.remove(id)).to be true
         end
 
         it 'fails if the user does not exist' do
-          expect(Users.remove(id + 1)).to be false
+          expect(Model::Users.remove(id + 1)).to be false
         end
 
-        it 'fails if a timecard belong to the user exists' do
+        it 'success even if a timecard belong to the user exists' do
           time = (Time.now).strftime('%X')
           day = Date.today
-          Timecard.create(day: day, user_id: id, attendance: time)
-          expect(Users.remove(id)).to be false
+          Model::Timecard.create(day: day, user_id: id, attendance: time)
+          expect(Model::Users.remove(id)).to be true
         end
       end
 
       context 'when getting the status' do
         it 'succeeds with a valid user' do
-          expect(Users.status(id)).not_to be nil
+          expect(Model::Users.status(id)).not_to be nil
         end
 
         it 'fails if the user does not exist' do
-          expect(Users.status(id + 1)).to be nil
+          expect(Model::Users.status(id + 1)).to be nil
         end
       end
     end
