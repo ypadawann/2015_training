df08ca65ed303a0cac35933b99e3fced37d22376
yasuhiro mori <yamori@debian.localdomain>
2015-05-22 17:43:56 +0900

    出退勤処理APIの修正
    ヘルパー利用
    エラー時の処理を先に記述

--- testapp/api/v1/timecard_api.rb
+++ testapp/api/v1/timecard_api.rb
@@ -7,89 +7,71 @@ module API
     class Timecards < Grape::API
       #prefix 'users'
       #resource '/:user_id' do
+
       resource 'users/:user_id' do
         
         post '/attend' do
           p 'attend test'
-          p user_id = params[:user_id]
-          if user_id == env['rack.session'][:no]
-            p time = (Time.now).strftime('%X')
-            p date = Date.today
-            if Model::Timecard_operation.attend(date, user_id, time) == true
-              p name = Model::Users.get_name(user_id)
-              p department = Model::Departments.name_of(Model::Users.get_department(user_id))
-              {user_id: user_id, name: name, 
-                department: department, date: date, attendance: time}
-            else
-              error!('already attend', 400)
-            end
-          else
-            p 'wrong id'
-            error!('failed authentication', 403)
+          p user_id = params[:user_id].to_i
+          authenticate!(user_id)
+          p time = (Time.now).strftime('%X')
+          p date = Date.today
+          if !Model::Timecard_operation.attend(date, user_id, time)
+            error!('already attend', 400)
           end
+          p name = Model::Users.get_name(user_id)
+          p department = Model::Departments.name_of(Model::Users.get_department(user_id))
+          {user_id: user_id, name: name, 
+            department: department, date: date, attendance: time}
         end
         
         put '/attend/:date' do
           p 'attend/date'
-          p  user_id = params[:user_id]
-          if user_id == env['rack.session'][:no]
-            p date = params[:date]
-            p time = params[:attendance]
-            Model::Timecard_operation.update_attend(date, user_id, time)
-            p name = Model::Users.get_name(user_id)
-            p department = Model::Departments.name_of(Model::Users.get_department(user_id))
-            {user_id: user_id, name: name, department: department}
-          else
-            error!('failed authentication', 403)
-          end
+          p  user_id = params[:user_id].to_i
+          authenticate!(user_id)
+          p date = params[:date]
+          p time = params[:attendance]
+          Model::Timecard_operation.update_attend(date, user_id, time)
+          p name = Model::Users.get_name(user_id)
+          p department = Model::Departments.name_of(Model::Users.get_department(user_id))
+          {user_id: user_id, name: name, department: department}
         end
         
         post '/leave' do
-          p user_id = params[:user_id]
-          if user_id == env['rack.session'][:no]
-            time = (Time.now).strftime('%X')
-            date = Date.today
-            if Model::Timecard_operation.returnhome(date, user_id, time) == true
-              p name = Model::Users.get_name(user_id)
-              p department = Model::Departments.name_of(Model::Users.get_department(user_id))
-              {user_id: user_id, name: name, 
-                department: department, date: date, leaving: time}
-            else
-              error!('wrong access', 400)
-            end
-          else
-            p 'wrong id'
-            error!('failed authentication', 403)
+          p user_id = params[:user_id].to_i
+          authenticate!(user_id)
+          time = (Time.now).strftime('%X')
+          date = Date.today
+          if !Model::Timecard_operation.returnhome(date, user_id, time)
+            error!('wrong access', 400)
           end
+          p name = Model::Users.get_name(user_id)
+          p department = Model::Departments.name_of(Model::Users.get_department(user_id))
+          {user_id: user_id, name: name, 
+            department: department, date: date, leaving: time}
         end
         
         put 'leave/:date' do
-          user_id = params[:user_id]
-          if user_id == env['rack.session'][:no]
-            date = params[:date]
-            time = params[:leaving]
-            Model::Timecard_operation.update_leave(date, user_id, time)
-            name = Model::Users.get_name(user_id)
-            department = Model::Departments.name_of(Model::Users.get_department(user_id))
-            {user_id: user_id, name: name, department: department}
-          else
-            error!('failed authentication', 403)
-          end
+          user_id = params[:user_id].to_i
+          authenticate!(user_id)
+          date = params[:date]
+          time = params[:leaving]
+          Model::Timecard_operation.update_leave(date, user_id, time)
+          name = Model::Users.get_name(user_id)
+          department = Model::Departments.name_of(Model::Users.get_department(user_id))
+          {user_id: user_id, name: name, department: department}
         end
                
         put '/attend-leave/:year_month' do
-          user_id = params[:user_id]
-          if user_id == env['rack.session'][:no]
-            year_month = params[:year_month]
-            timecard_data = params[:data]
-            timecard_data[0].day
-            Model::Timecard_operation.update_all(year_month, timecard_data, user_id)
-            name = Model::Users.get_name(user_id)
-            department = Model::Departments.name_of(Model::Users.get_department(user_id)) 
-            {user_id: user_id, name: name, department: department}
-          else
-            error!('failed authentication', 403)
-          end
+          user_id = params[:user_id].to_i
+          authenticate!(user_id)  
+          year_month = params[:year_month]
+          timecard_data = params[:data]
+          timecard_data[0].day
+          Model::Timecard_operation.update_all(year_month, timecard_data, user_id)
+          name = Model::Users.get_name(user_id)
+          department = Model::Departments.name_of(Model::Users.get_department(user_id)) 
+          {user_id: user_id, name: name, department: department}
         end
         
       end
