c5a7388de0b944bc81e5cd47f96b93b2fd46d072
ypadawann <yamori@debian.localdomain>
2015-06-05 11:03:05 +0900

    adminのログイン状態セッションのキー値を変更
    login_status => admin_login
    adminだと分かりやすくするため

--- testapp/api/v1/adminutils.rb
+++ testapp/api/v1/adminutils.rb
@@ -2,7 +2,7 @@ module API
   module V1
     module AdminUtlis     
       def session_check()
-        if !env['rack.session'][:login_status]
+        if !env['rack.session'][:admi_login]
           error!('Not Found', 404)
         end
       end
