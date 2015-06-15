736b63f7c44cc028dd749df6f6647af1938d319f
ko <ko@khenmi.mydomain>
2015-06-15 13:39:52 +0900

    ユーザ登録画面のcucumberテスト

---
+++ testapp/features/step_definitions/register.rb
@@ -0,0 +1,39 @@
+前提(/^(?::\s*)?部署 (.*?) が存在(?:し|する)$/) do |department_name|
+  visit "/test/add_department/#{department_name}"
+end
+
+もし(/^.*?\((.*?)\) 画面にアクセスした?$/) do |path|
+  visit path
+end
+
+もし(/^.*?\((.*?)\) 欄に (.*?) を入力した?$/) do |element_id, value|
+  page.find(:css, element_id).set(value)
+end
+
+もし(/^.*?\((.*?)\) 欄から (.*?) を選択した?$/) do |element_id, value|
+  page.find(:css, element_id).find(:option, value).select_option
+end
+
+もし(/^.*?\((.*?)\) ボタンをクリックした?$/) do |element_id|
+  page.find(:css, element_id).click
+end
+
+ならば(/^.*?\((.*?)\) 画面に遷移(?:し|する)$/) do |path|
+  expect(page.current_path).to eq(path)
+end
+
+ならば(/^.*?\((.*?)\) 欄に (.*?) が表示される?$/) do |element_id, value|
+  expect(page.find(element_id).text).to eq(value)
+end
+
+ならば(/^HTTPステータスコードは (.*?) であ(?:り|る)$/) do |status|
+  expect(page.status_code).to eq(status.to_i)
+end
+
+ならば(/^スクリーンショットを保存(?:し|する)$/) do
+  save_screenshot('screen_shot.png')
+end
+
+ならば(/^ページを保存(?:し|する)$/) do
+  save_page('page.html')
+end
