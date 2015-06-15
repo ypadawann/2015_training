7b43f2c8c4325548102bd39720c497fad45893dc
ko <ko@khenmi.mydomain>
2015-06-15 13:37:36 +0900

    cucumber起動時に環境をtestに設定

--- testapp/features/support/env.rb
+++ testapp/features/support/env.rb
@@ -5,6 +5,7 @@ require 'database_cleaner'
 require 'database_cleaner/cucumber'
 require 'active_record'
 
+ENV['RACK_ENV'] ||= 'test'
 DatabaseCleaner.strategy = :truncation
 
 Capybara.app = eval("Rack::Builder.new {( " + File.read(File.dirname(__FILE__) + '/../../config.ru') + "\n )}")
