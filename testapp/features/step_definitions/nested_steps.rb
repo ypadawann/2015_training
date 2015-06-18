cbfc7165464341b52212ee56cf86649decf0c605
ko <ko@khenmi.mydomain>
2015-06-18 16:27:37 +0900

    処理を待ってsleepするステップで秒数を固定でなく指定できるように

--- testapp/features/step_definitions/nested_steps.rb
+++ testapp/features/step_definitions/nested_steps.rb
@@ -7,7 +7,6 @@ end
   switch_to_window(window_opened_by { step nested_step })
 end
 
-もし(/^(.*) 通信を待(?:ち|った)$/) do |nested_step|
-  step nested_step
-  sleep 1
+もし(/^(\d+\.\d*) 秒待(?:ち|った)$/) do |sec|
+  sleep sec.to_f
 end
