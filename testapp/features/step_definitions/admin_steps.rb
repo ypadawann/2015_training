7742b765fac8c60fb0cd86c5785e6033dc063de8
ypadawann <yamori@debian.localdomain>
2015-06-22 12:54:14 +0900

    selectフォームに関するテストコードを修正

--- testapp/features/step_definitions/admin_steps.rb
+++ testapp/features/step_definitions/admin_steps.rb
@@ -26,9 +26,10 @@ end
 
 もし(/^".*?\((.*?)\)" で "(.*?)" を選択$/) do |select_obj, target_opt|
   box = page.find(select_obj)
-  box.select target_opt
+  box.select(target_opt)
 end
 
+ 
 もし(/^".*?\((.*?)\)" が存在$/) do |obj|
   page.find(obj)
 end
@@ -44,9 +45,13 @@ end
   end
 end
 
-もし(/^".*?\((.*?)\)" で "(.*?)" が選択$/) do |obj, str|
+もし(/^".*?\((.*?)\)" で "(.*?)" が選択$/) do |element_id, str|
   wait_for_ajax
-  page.find(obj).has_select?(:selected => str)
+  #p page.has_select?(element_id, selected: value)
+  #obj = page.find(:xpath, "//select[@id='select-department']/option[@selected='selected']")
+  select_element = page.find(element_id)
+  selected_id = select_element.value
+  expect(select_element.find(:xpath, "//option[@value=#{selected_id}]").text).to eq(str)
 end
 
 もし(/^"(.*?)" 秒待機$/) do | second |
