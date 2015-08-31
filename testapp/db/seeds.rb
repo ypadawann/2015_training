de5c6173783abfad43425f675681ec6ea00aae24
ko <ko@khenmi.local>
2015-08-31 16:50:18 +0900

    初期データとしてID: admin, PASS: passwordの管理者と部署一つを用意

---
+++ testapp/db/seeds.rb
@@ -0,0 +1,3 @@
+
+Model::Admin.create(id: 'admin', password: '7973d2f7b0b06d7d8dd2065a0efdc96752dc1f71ad2ba4bf2d672c2070126fb1$579jVZaUSjfAhx7aYcG7Bo9h8nss40D4')
+Model::Department.create(name: 'ITセキュリティ開発部')
