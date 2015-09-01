50baa40566e9371630175d7c85b2defa4eba62ec
ko <ko@khenmi.local>
2015-09-01 15:53:57 +0900

    unicornで使うディレクトリを/home/training以下でなく/var以下に
    ユーザ名を書いておきたくないので

--- testapp/unicorn.rb
+++ testapp/unicorn.rb
@@ -1,6 +1,6 @@
 # set path to app that will be used to configure unicorn,
 # note the trailing slash in this example
-@dir = '/home/training/2015_training/testapp/'
+@dir = File.dirname(__FILE__)
 
 worker_processes 2
 working_directory @dir
@@ -9,11 +9,11 @@ timeout 30
 
 # Specify path to socket unicorn listens to,
 # we will use this in our nginx.conf later
-listen "#{@dir}tmp/sockets/unicorn.sock", :backlog => 64
+listen "/var/tmp/unicorn.sock", :backlog => 64
 
 # Set process id path
-pid "#{@dir}tmp/pids/unicorn.pid"
+pid "/var/run/unicorn.pid"
 
 # Set log file paths
-stderr_path "#{@dir}log/unicorn.stderr.log"
-stdout_path "#{@dir}log/unicorn.stdout.log"
+stderr_path "/var/log/unicorn.stderr.log"
+stdout_path "/var/log/unicorn.stdout.log"
